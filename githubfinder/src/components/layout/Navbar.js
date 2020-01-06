0015c71ed829f9c73dbe316a1a1d5a26b323eb9e
Tobi Adeyeye <oadeyey@okstate.edu>
2020-01-05 20:41:19 -0600

    Latest Commit 1/5/2020

--- githubfinder/src/components/layout/Navbar.js
+++ githubfinder/src/components/layout/Navbar.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 const Navbar = props => {
   return (
@@ -8,13 +9,21 @@ const Navbar = props => {
         <i className={props.icon}></i>
         {props.title}
       </h1>
+      <ul>
+        <li>
+          <Link to='/'>Home</Link>
+        </li>
+        <li>
+          <Link to='/about'>About</Link>
+        </li>
+      </ul>
     </nav>
   );
 };
 
 Navbar.defaultProps = {
-  title: 'Github Finder',
-  icon: 'fab fa-github'
+  title: "Github Finder",
+  icon: "fab fa-github"
 };
 
 Navbar.propTypes = {
